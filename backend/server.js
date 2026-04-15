ea3db7e775efee8a1e0ea597185c42fe588d0636
sayak53 <sayakchakraborty928@gmail.com>
2026-04-15 14:08:38 +0530

    completed crud opertions using thunder client

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.get("/", (req, res) => {
 
 // MongoDB connection
 mongoose.connect("mongodb://127.0.0.1:27017/studentDB")
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.log(err));
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
 app.listen(5000, () => console.log("Server running on port 5000"));
