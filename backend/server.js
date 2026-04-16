c1348ee68123fd8dd1b4624c1ba7e725806de07f
sayak53 <sayakchakraborty928@gmail.com>
2026-04-16 20:12:53 +0530

    fixing for deployment

--- backend/server.js
+++ backend/server.js
@@ -15,9 +15,14 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-// MongoDB connection
-mongoose.connect("mongodb://127.0.0.1:27017/studentDB")
-  .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log(err));
+// MongoDB connection (USE ENV VARIABLE)
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("MongoDB Connected"))
+.catch(err => console.log(err));
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+// Port fix for Render
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server running on port " + PORT));
